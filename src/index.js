98d7a9e9a52037434c01bb4d93c47517d296ca68
Craig5117 <craig.bennett5117@gmail.com>
2021-07-01 15:17:17 -0400

    select dropdown working and styled

--- src/index.js
+++ src/index.js
@@ -6,36 +6,71 @@ registerBlockType('plugin/phone-link', {
   category: 'common',
   attributes: {
     telNumber: { type: 'string' },
-    telMessage: { type: 'string' }
+    telMessage: { type: 'string' },
   },
   edit: (props) => {
     function updateTelNumber(event) {
       props.setAttributes({ telNumber: event.target.value });
     }
 
+    function handleSelect(event) {
+      props.setAttributes({ telMessage: event.target.value });
+    }
+
     return (
       <>
-        <p>
+        <p style={{fontSize: ".75em"}}>
           Enter the phone number below as a 10 digit number with no other
-          characters or spaces. E.g. 7778675309.
+          characters or spaces (e.g. 7778675309).<br/>
+          Select how you want to display the number type (e.g. Mobile: (777)...)<br/>
+          If you select none, only the number link will display.
         </p>
+        <select
+          name="phone-type-select"
+          id="phone-type-select"
+          onChange={handleSelect}
+          style={{marginRight: "2em", verticalAlign: "bottom"}}
+        >
+          <option value="">none</option>
+          <option value="Mobile:">Mobile:</option>
+          <option value="Telephone:">Telephone:</option>
+          <option value="Tel:">Tel:</option>
+          <option value="Phone:">Phone:</option>
+        </select>
         <input
           type="text"
           placeholder="Enter Phone Number..."
           value={props.attributes.telNumber}
           onChange={updateTelNumber}
+          style={{verticalAlign: "bottom"}}
         />
+        
       </>
     );
   },
   save: (props) => {
-      const numbersArr = props.attributes.telNumber.split('');
-      const formattedNumber = `(${numbersArr[0]}${numbersArr[1]}${numbersArr[2]}) ${numbersArr[3]}${numbersArr[4]}${numbersArr[5]}-${numbersArr[6]}${numbersArr[7]}${numbersArr[8]}${numbersArr[9]}`;
+    const numbersArr = props.attributes.telNumber.split('');
+    const formattedNumber =
+      numbersArr.length === 10
+        ? `(${numbersArr[0]}${numbersArr[1]}${numbersArr[2]}) ${numbersArr[3]}${numbersArr[4]}${numbersArr[5]}-${numbersArr[6]}${numbersArr[7]}${numbersArr[8]}${numbersArr[9]}`
+        : null;
     return (
       <p className="phone-link-container">
-        Tel: <span itemprop="telephone">
-          <a className="phone-link" href={`tel:+1${props.attributes.telNumber}`}>{formattedNumber}</a>
-        </span>
+        {props.attributes.telMessage && (
+          <span className="phone-link-message">{props.attributes.telMessage} </span>
+        )}
+        {formattedNumber ? (
+          <span itemprop="telephone">
+            <a
+              className="phone-link"
+              href={`tel:+1${props.attributes.telNumber}`}
+            >
+              {formattedNumber}
+            </a>
+          </span>
+        ) : (
+          <span>invalid number</span>
+        )}
       </p>
     );
   },
